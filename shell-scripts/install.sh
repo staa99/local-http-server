1697cea67451f222abd255c27dca6809f98612dc
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2025-04-06 00:31:52 +0300

    improve script configuration

--- shell-scripts/install.sh
+++ shell-scripts/install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+config_dir=${LHS_CONFIG_DIR:-~/.local-http-server}
+host_port=${LHS_PORT:-80}
+config_file_name=config.json
+config_file="$config_dir"/"$config_file_name"
+install_dir=${LHS_INSTALL_DIR:-~/.local-http-server/bin}
+launch_script="$install_dir/start.sh"
+
 # Check if jq is installed
 if ! command -v jq &> /dev/null; then
     echo "jq could not be found. Follow the instructions https://jqlang.org/download/ to install it."
@@ -25,15 +32,15 @@ fi
 echo "Successfully pulled the latest local-http-server Docker image."
 
 # Download the start script
-mkdir -p ~/.local-http-server/bin
-if ! curl -sSL https://raw.githubusercontent.com/staa99/local-http-server/refs/tags/v1.0.0-beta.1/shell-scripts/start.sh > ~/.local-http-server/bin/start.sh; then
+mkdir -p "$install_dir"
+if ! curl -sSL https://raw.githubusercontent.com/staa99/local-http-server/refs/tags/v1.0.0-beta.2/shell-scripts/start.sh > "$launch_script"; then
     echo "Failed to download the start script. Please check your internet connection."
     exit 1
 fi
 
 # Make the start script executable
-if [ -f ~/.local-http-server/bin/start.sh ]; then
-    chmod +x ~/.local-http-server/bin/start.sh
+if [ -f "$launch_script" ]; then
+    chmod +x "$launch_script"
 else
     echo "The start script was not downloaded successfully."
     exit 1
@@ -44,20 +51,24 @@ user_shell=$(basename "$SHELL")
 
 # add an alias for local-http-server to the user's shell configuration file
 echo "# Added by local-http-server installation script
-alias local-http-server='~/.local-http-server/bin/start.sh'" >> "$HOME/.$user_shell"rc
+alias local-http-server='$launch_script'" >> "$HOME/.$user_shell"rc
 
-source ~/.local-http-server/bin/start.sh
+source "$launch_script"
 
 echo "
 local-http-server installation completed successfully.
 
-The configuration in stored at ~/.local-http-server/config.json
+The configuration in stored at $config_file
 
-Register services by adding ports in the config.json file under the 'ports' section. For example:
+Register services by adding ports in the config file under the 'ports' section. For example:
 
 {
   \"ports\": {
     \"lhs-test-service\": \"55455\"
   }
-}
+}.
+
+Verify your installation by running
+
+  curl http://localhost:$host_port
 "
--- shell-scripts/start.sh
+++ shell-scripts/start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-config_file=${LHS_CONFIG:-~/.local-http-server/config.json}
+config_dir=${LHS_CONFIG_DIR:-~/.local-http-server}
 host_port=${LHS_PORT:-80}
+config_file_name=config.json
+config_file="$config_dir"/"$config_file_name"
 
-mkdir -p ~/.local-http-server
+mkdir -p "$config_dir"
 
 if [ ! -f "$config_file" ] || [ ! -s "$config_file" ]; then
     echo '{}' > "$config_file"
@@ -28,27 +30,28 @@ docker rm local-http-server > /dev/null 2>&1 || true
 echo "Starting local-http-server Docker container..."
 docker run -d \
   --name local-http-server \
-  -p $host_port:80 \
-  -v ~/.local-http-server:/app \
+  -p "$host_port":80 \
+  -v "$config_dir":/app \
+  -e LHS_CONFIG_FILE="$config_file_name" \
   staa99/local-http-server:latest
 
-host=$(docker container exec local-http-server sh -c "ping -c 1 host.docker.internal" | awk -F'[()]' '/PING/{print $2}')
+if jq empty "$config_file" > /dev/null 2>&1; then
+  # Use jq to set the ports.lhs-test-service to "55455" if it's not set
+  # This is set as a sample for configuration
+  if ! jq -e '.ports."lhs-test-service"? // empty' "$config_file" | grep -q '.'; then
+    jq '.ports."lhs-test-service"="55455"' "$config_file" > tmp.$$ && mv tmp.$$ "$config_file"
+  fi
 
-if [ -n "$host" ]; then
-    if jq empty "$config_file" > /dev/null 2>&1; then
-        # Use jq to update the JSON file
-        jq --arg host "$host" '.static.host_ip = $host' "$config_file" > tmp.$$.json && mv tmp.$$.json "$config_file"
-
-        # Use jq to set the ports.lhs-test-service to "55455" if it's not set
-        # This is set as a sample for configuration
-        if ! jq -e '.ports."lhs-test-service"? // empty' 'config.json' | grep -q '.'; then
-          jq '.ports."lhs-test-service"="55455"' 'config.json' > tmp.$$ && mv tmp.$$ 'config.json'
-        fi
-
-        echo "Updated static.host_ip to $host in $config_file"
-    else
-        echo "$config_file is not valid JSON. Please fix the file and try again."
-    fi
+  host=$(docker container exec local-http-server sh -c "ping -c 1 host.docker.internal" | awk -F'[()]' '/PING/{print $2}')
+  if [ -n "$host" ]; then
+    # Use jq to update the JSON file
+    jq --arg host "$host" '.static.host_ip = $host' "$config_file" > tmp.$$.json && mv tmp.$$.json "$config_file"
+    echo "Updated static.host_ip to $host in $config_file"
+  else
+    echo "Failed to determine host IP. Configure the host IP address manually"
+  fi
 else
-    echo "Failed to determine host IP."
+    echo "$config_file is not valid JSON. Please fix the file and try again."
 fi
+
+
