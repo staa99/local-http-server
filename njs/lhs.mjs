d2c6d1cad54ff6d7900844ddc2d5b20774020247
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2025-04-05 18:12:07 +0300

    fix json responses

--- njs/lhs.mjs
+++ njs/lhs.mjs
@@ -10,91 +10,57 @@ async function proxy(r) {
 
   if (!service) {
     const config = await loadConfig();
-    r.return(
-      200,
-      JSON.stringify({
-        message:
-          "Local HTTP server is running. Please specify a valid URI to route to the respective service",
-        config,
-      }),
-    );
+    return jsonResponse(r, 200, {
+      message:
+        "Local HTTP server is running. Please specify a valid URI to route to the respective service",
+      config,
+    });
   }
 
   const port = await getPortNumber(service);
   if (!port) {
     r.error(`LHS_LOG_LINE: ${service} is not configured`);
-    r.return(
-      404,
-      JSON.stringify({
-        error: "Not Found",
-        message: `No backend configured for ${service}. Please check your configuration.`,
-      }),
-    );
-    return;
+    return jsonResponse(r, 404, {
+      status: "failed",
+      message: `No backend configured for ${service}. Please check your configuration.`,
+    });
   }
 
-  const serviceUrl = `/internal-service/${port}/${hostIp}${path}`; // Construct the service
-  r.log("LHS_LOG_LINE: Proxying request to " + serviceUrl); // Log the proxy action
+  const serviceUrl = `/internal-service/${port}/${hostIp}${path}`;
+  r.log("LHS_LOG_LINE: Proxying request to " + serviceUrl);
   r.internalRedirect(serviceUrl, r.requestBuffer);
 }
 
 async function register(r) {
-  const body = JSON.parse(r.requestText);
+  const body = getRegisterPayload(r);
+  if (!body) {
+    // the response is handled already
+    return;
+  }
 
   // Lock the file
   const fd = fs.openSync(configFile, "r+");
-
   try {
     const config = await loadConfig();
+    if (!config.ports) {
+      config.ports = {};
+    }
 
-    fs.writeFileSync(
-      configFile,
-      JSON.stringify({
-        ...config,
-        ports: {
-          ...config.ports,
-          // Register the new service and port
-          [body.service]: body.port,
-        },
-      }),
-    );
-
+    config.ports[body.service] = body.port;
+    fs.writeFileSync(configFile, JSON.stringify(config));
     r.log(
-      "LHS_LOG_LINE: Registered new service " +
-        body.service +
-        " on port " +
-        body.port,
+      `LHS_LOG_LINE: Registered service ${body.service} on port ${body.port}`,
     );
 
     // Update the shared memory for the new service
     ngx.shared.ports.set(body.service, body.port);
-
-    r.return(
-      200,
-      JSON.stringify({
-        status: "successful",
-        message: `${body.service} is now registered on ${body.port}`,
-      }),
-    );
+    return jsonResponse(r, 200, {
+      status: "successful",
+      message: `${body.service} is now registered on port ${body.port}`,
+    });
   } finally {
     // Unlock the file
     fs.closeSync(fd);
-  }
-}
-
-async function config() {
-  try {
-    const config = await loadConfig();
-    return {
-      status: "successful",
-      config,
-    };
-  } catch (error) {
-    ngx.log(ngx.ERROR, "LHS_LOG_LINE: Failed to load configuration - ", error);
-    return {
-      status: "failed",
-      message: "Unable to load configuration",
-    };
   }
 }
 
@@ -120,7 +86,7 @@ async function getHostIp() {
 
 async function loadConfig() {
   const configText = await fs.promises.readFile(configFile, "utf8");
-  const config = JSON.parse(configText);
+  const config = JSON.parse(configText || '{}');
 
   const zones = Object.entries(config);
   for (let i = 0; i < zones.length; i++) {
@@ -129,11 +95,56 @@ async function loadConfig() {
 
     for (let i = 0; i < zoneConfig.length; i++) {
       const entry = zoneConfig[i];
-      ngx.shared[zone].set(entry[0], entry[1]);
+      ngx.shared[zone].set(entry[0].toString(), entry[1].toString());
     }
   }
 
   return config;
 }
 
-export default { proxy };
+function getRegisterPayload(r) {
+  let body;
+  try {
+    body = JSON.parse(r.requestText);
+  } catch (e) {
+    r.error(`LHS_LOG_LINE: Invalid request body: ${r.requestText}`);
+    return jsonResponse(r, 400, {
+      status: "failed",
+      message: "Invalid request body. Please provide a valid JSON.",
+    });
+  }
+
+  if (!body.service || !/^([a-zA-Z0-9_-]+)$/.test(body.service)) {
+    r.error(
+      `LHS_LOG_LINE: Invalid service name: ${body.service}. Service names can only contain alphanumeric characters, hyphens, and underscores.`,
+    );
+    return jsonResponse(r, 400, {
+      status: "failed",
+      message:
+        "Invalid service name. Service names can only contain alphanumeric characters, hyphens, and underscores.",
+    });
+  }
+
+  const port = Number(body.port);
+  if (Number.isNaN(port) || port <= 0 || port > 65535) {
+    r.error(
+      `LHS_LOG_LINE: Invalid port number: ${body.port}. Port must be a number between 1 and 65535.`,
+    );
+    return jsonResponse(r, 400, {
+      status: "failed",
+      message:
+        "Invalid port number. Port must be a number between 1 and 65535.",
+    });
+  }
+
+  body.port = port.toString(10);
+  return body;
+}
+
+function jsonResponse(r, status, body) {
+  r.headersOut["Content-Type"] = "application/json";
+  body = JSON.stringify(body);
+  r.return(status, body);
+}
+
+export default { proxy, register };
